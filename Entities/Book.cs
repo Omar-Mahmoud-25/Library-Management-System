06fffa5cf1342b9ff1f98442fad9bb9c8e28d718
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2025-10-03 18:31:55 +0300

    added book Model

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -1,4 +1,23 @@
-class Book
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagementSystem.Entities;
+
+public class Book
 {
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required, MaxLength(100)]
+    public string Title { get; set; }
+    [Required, MaxLength(100)]
+    public string Author { get; set; }
+
+    [MaxLength(1000)]
+    public string Discription { get; set; }
+    [Required]
+    public DateTime PublishedDate { get; set; }
+    public int CopiesAvailable { get; set; } = 1;
+    [Required ,Url]
+    public string CoverImageUrl { get; set; }
 }
---
+++ Readme.md
@@ -0,0 +1,15 @@
+
+- login
+- sign up
+
+
+- book details (borrow / return / delete(for admin))
+- available books
+- home page
+
+for admin:
+- list users / admins (admin)
+- add / edit / delete book
+
+for students:
+- borrowed books
